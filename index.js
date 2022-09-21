edb68128c6f2a58d9214724165f05e48c022a025
abelmore33 <belmoreaaron9@gmail.com>
2022-09-21 09:32:18 -0400

    completed tasks 1d

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ Do the following:
 function multiply(num1, num2){
   return num1 * num2;
 }
+console.log('task 1d',multiply(10,10));
 
 
 
@@ -227,9 +228,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
-}
+function grade(score){
+  if(score >= 90 && score <=100){
+    return "you got an A";
+  }else if(score >= 80 && score <= 89){
+    return "you got a B";
+  }else if(score >=70 && score <=79){
+    return "you got a C";
+  }else if(score >= 60 && score <= 69){
+    return "you got a D";
+  }else if(score < 60){
+    return "you got an F";
+  }
+  }
+  console.log('task 7',grade(64));
+
 
 
 
